ed059c4c7677502c3f0322c9257e347e818e96a8
Paul Li <pli283@uwo.ca>
2017-11-06 01:36:44 -0500

    added hangman + added light db

--- Light.h
+++ Light.h
@@ -5,7 +5,8 @@
 
 #include <string>
 #include <iostream>
-
+#include <Wt/Dbo/Types>
+#include <Wt/Dbo/WtSqlTraits>
 
 class Light {
 	//Light data members (name, type, status (on/off), brightness, hue, saturation, transition time)
@@ -22,105 +23,28 @@ public:
 	// Parameters: name, type, status, brightness, hue, saturation, transition time
 	// Return: none
 	// Description: constructor 
-	Light(std::string nName, std::string nType, std::string nStatus, int nBri, int nHue, int nSat, int nTrans) {
-		name = nName;
-		type = nType;
-		status = nStatus;
-		bri = nBri;
-		hue = nHue;
-		sat = nSat;
-		transitionTime = nTrans;
-	}
+	Light(std::string nName, std::string nType, std::string nStatus, int nBri, int nHue, int nSat, int nTrans);
 
+	Light();
 	// Function Name: ~Light() 
 	// Parameters: 
 	// Return: none
 	// Description: destructor
 	~Light() {}
 
-	// Function Name: getName()
-	// Parameters: None
-	// Return: string of the name
-	// Description: returns the name
-	std::string getName(void) {
-		return name;
-	}
+	int getBri();
 
-	// Function Name: getType()
-	// Parameters: None
-	// Return: string of the type
-	// Description: returns the type
-	std::string getType(void) {
-		return type;
-	}
 
-	// Function Name: getStatus()
-	// Parameters: None
-	// Return: bool of the status
-	// Description: returns the status
-	std::string getStatus(void) {
-		return status;
-	}
-
-	// Function Name: getBrightness()
-	// Parameters: None
-	// Return: int of the brightness
-	// Description: returns the brightness
-	int getBrightness(void) {
-		return bri;
-	}
-
-	// Function Name: getHue()
-	// Parameters: None
-	// Return: int of the hue
-	// Description: returns the hue
-	int gethue(void) {
-		return hue;
-	}
-
-	// Function Name: getSaturation()
-	// Parameters: None
-	// Return: int of the saturation
-	// Description: returns the saturation
-	int getSaturation(void) {
-		return sat;
-	}
-
-	// Function Name: getTransition()
-	// Parameters: None
-	// Return: int of the transition time
-	// Description: returns the transition time
-	int getTransition(void) {
-		return transitionTime;
-	}
-
-	void setName(std::string newName) {
-		name = newName;
-	}
-
-	void setType(std::string newType) {
-		type = newType;
-	}
-
-	void setStatus(bool newStatus) {
-		status = newStatus;
-	}
-
-	void setBrightness(int newBri) {
-		bri = newBri;
-	}
-
-	void setHue(int newHue) {
-		hue = newHue;
-	}
-
-	void setSaturation(int newSat) {
-		sat = newSat;
-	}
-
-	void setTransition(int newTrans) {
-		transitionTime = newTrans;
-	}
+	  template<class Action>
+	  void persist(Action& a)
+	  {
+	    Wt::Dbo::field(a, name, "name");
+	    Wt::Dbo::field(a, type, "type");
+	    Wt::Dbo::field(a, status, "status");
+	    Wt::Dbo::field(a, bri, "bri");
+	    Wt::Dbo::field(a, hue, "hue");
+	    Wt::Dbo::field(a, transitionTime, "transitionTime");
+	  }
 };
 
 #endif	/* LIGHT_H_ */
