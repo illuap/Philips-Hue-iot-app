e755054fd12ad189c791f06479fe835258c7d565
Paul Li <pli283@uwo.ca>
2017-11-08 02:29:49 -0500

    added db functionality

--- Bridge.h
+++ Bridge.h
@@ -1,5 +1,18 @@
+#pragma once
+#ifndef BRIDGE_H
+#define BRIDGE_H
+
 #include <string>
 #include <vector>
+
+#include <Wt/Dbo/Types>
+#include <Wt/Dbo/WtSqlTraits>
+
+#include "Light.h"
+
+class Light;
+//typedef Wt::Dbo::collection< Wt::Dbo::ptr<Light> > Lights;
+
 class Bridge {
 private:
 	std::string bridgeName;
@@ -9,10 +22,22 @@ private:
 	std::string userId;
 	bool registered;
 	int portNumber;
-	std::vector<Group> groupList;
-	std::vector<Light> lightList;
+//	std::vector<Group> groupList;
+//	std::vector<Light> lightList;
+	//Wt::Dbo::collection<Wt::Dbo::ptr<Light> > lights;
 
 public:
+	Bridge() {
+		bridgeName = "";
+		location= "";
+		ipAddress= "";
+		hostName= "";
+		userId= "";
+		registered= false;
+		portNumber= 0;
+	}
+	~Light() {}
+
 	std::string getBridgeName() {
 		return bridgeName;
 	}
@@ -37,11 +62,11 @@ public:
 	}
 
 	int getPortNumber() {
-		return portNumber
+		return portNumber;
 	}
 
 	void setBridgeName(std::string newName) {
-		bridgeName = name;
+		bridgeName = newName;
 	}
 
 	void setLocation(std::string newLocation) {
@@ -61,12 +86,16 @@ public:
 	}
 
 	void setRegistered(bool newRegistered) {
+<<<<<<< HEAD
+		registered = newRegistered;
+=======
 		register = newRegistered;
+>>>>>>> 0174b99633680a53936e8dc2eff092871dc65ce6
 	}
 	void setPortNumber(int newPortNumber) {
 		portNumber = newPortNumber;
 	}
-
+/*
 	void addGroup(Group newGroup) {
 		groupList.push_back(newGroup);
 	}
@@ -78,4 +107,18 @@ public:
 	void removeGroup(Group group) {
 
 	}
+*/
+	template<class Action>
+	void persist(Action& a)
+	{
+		Wt::Dbo::field(a, bridgeName, "bridgeName");
+		Wt::Dbo::field(a, location, "location");
+		Wt::Dbo::field(a, ipAddress, "ipAddress");
+		Wt::Dbo::field(a, hostName, "hostName");
+		Wt::Dbo::field(a, userId, "userId");
+		Wt::Dbo::field(a, registered, "registered");
+		Wt::Dbo::field(a, portNumber, "portNumber");
+		//Wt::Dbo::hasMany(a, lights, Wt::Dbo::ManyToOne, "bridgeLights");
+	}
 };
+#endif	/* BRIDGE_H */
