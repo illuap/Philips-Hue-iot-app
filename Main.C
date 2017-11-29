b52185ecd2046fab15bd15ebbfc169a7491dbaf1
Paul Li <pli283@uwo.ca>
2017-11-28 23:41:29 -0500

    added comments for main

--- Main.C
+++ Main.C
@@ -1,3 +1,16 @@
+/** @file Main.C
+*  @brief The main to run the server
+*
+*   It includes the bare essentials to start our server and the resources
+*   that are used in our application.
+* 
+*  @author Nicole Chow
+*  @author Paul Li
+*  @author Weija Zhou
+*  @author Daniel Le
+*  @date Nov 28, 2017
+*/
+
 #include <Wt/WApplication>
 #include <Wt/WServer>
 #include <Wt/WAnchor>
@@ -5,6 +18,12 @@
 #include "HueApp.h"
 #include "Session.h"
 
+/** @brief Create the application that is based off wt.
+ *
+ *  Creates the application and addes the resources that will be used in the application
+ *  
+ *  @param env the Wt enviornment.
+ */
 Wt::WApplication *createApplication(const Wt::WEnvironment& env)
 {
   Wt::WApplication *app = new Wt::WApplication(env);
@@ -23,7 +42,10 @@ Wt::WApplication *createApplication(const Wt::WEnvironment& env)
   return app;
 }
 
-
+/** @brief Starting our wt application.
+ *
+ *  The main function is to simple start our server by creating our wt application
+ */
 int main(int argc, char **argv)
 {
   try {
