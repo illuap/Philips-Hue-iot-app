0def555128a4a2019ee44c84d15c29c44369b624
Paul Li <pli283@uwo.ca>
2017-11-28 23:42:58 -0500

    added some files for doxgen

---
+++ html/search/files_7.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['session_2ec',['Session.C',['../Session_8C.html',1,'']]],
+  ['session_2eh',['Session.h',['../Session_8h.html',1,'']]],
+  ['singlegroupscontrol_2ec',['SingleGroupsControl.C',['../SingleGroupsControl_8C.html',1,'']]],
+  ['singlegroupscontrol_2eh',['SingleGroupsControl.h',['../SingleGroupsControl_8h.html',1,'']]]
+];
